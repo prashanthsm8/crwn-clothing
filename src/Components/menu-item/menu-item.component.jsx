7dca9b7ed3a2cb344b3e3460233862d842ecec18
Prashanth SM <prashanthsm8@gmail.com>
2020-03-10 22:19:31 +0530

    adding new files

---
+++ src/Components/menu-item/menu-item.component.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import './menu-item.component.scss'
+
+const MenuItem = ({title,imageUrl,size})=> 
+(
+    <div  style={
+        {backgroundImage:`url(${imageUrl})`}
+        } className={`${size} menu-item`}>
+    <div className='content'>
+        <h1 className='title'>{title.toUpperCase()}</h1>
+        <span className='subtitle'>Shop Now</span>
+    </div>
+    </div>
+)
+
+export default MenuItem;
---
+++ src/Pages/homepage.component.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './homepage.style.scss'
+import Directory from '../Components/directory/directory.component'
+
+const Homepage  = () => {
+
+    return (
+        <div className='homepage'>
+            <Directory></Directory>
+        </div>
+    )
+}
+
+export default Homepage;
