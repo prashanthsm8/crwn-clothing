8a297a829693848e0f25d08e72833589d62ba5bb
Prashanth SM <prashanthsm8@gmail.com>
2020-03-29 23:43:25 +0530

    edasd

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
 import React from 'react';
 import './App.css';
+import {Switch ,Route} from 'react-router-dom';
 import Homepage from '././Pages/homepage.component';
+
+const Hatspage = ()=>
+(
+  <div>
+    <h1>Hatspage</h1>
+  </div>
+);
 
 function App() {
   return (
     <div>
-      <Homepage></Homepage>
+      <Switch>
+      <Route exact path='/' component={Homepage}></Route>
+      <Route  path='/shop/hats' component={Hatspage}></Route>
+      </Switch>
     </div>
   );
 }
--- src/Components/directory/directory.component.jsx
+++ src/Components/directory/directory.component.jsx
@@ -51,9 +51,9 @@ class Directory extends React.Component
     {
         return(
             <div className='directory-menu'>
-                {this.state.sections.map(({id,title,imageUrl,size}) => (
+                {this.state.sections.map(({id, ...otherSectionProps}) => (
 
-                        <MenuItem key={id} title={title} imageUrl={imageUrl} size={size}></MenuItem>
+                        <MenuItem key={id} {...otherSectionProps} ></MenuItem>
                     )
                 )}
             </div>
