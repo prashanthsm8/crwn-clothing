1594f4e871128cc841902ed94fadb8a60cc9d19e
prashanthsm8 <37961439+prashanthsm8@users.noreply.github.com>
2020-05-12 22:56:04 +0530

    n

--- src/Components/sign-in/sign-in.component.jsx
+++ src/Components/sign-in/sign-in.component.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import './sign-in.styles.scss';
 import FormInput from '../form-input/form-input.jsx'
 import CustomButton from '../custom-button/cutom-button.component.jsx'
-import {signInWithGoogle} from '../../firebase/firebase.utils.js'
+import {auth, signInWithGoogle} from '../../firebase/firebase.utils.js'
 
 class SignIn extends React.Component
 {
@@ -16,17 +16,26 @@ class SignIn extends React.Component
         }
     }
 
-    handleSubmit = event =>
+    handleSubmit = async event =>
     {
         event.preventDefault();
-        this.setState({email : '', password: ''})
+        const {email, password} = this.state;
+
+        try{
+            await auth.signInWithEmailAndPassword(email,password);
+            this.setState({email : '', password: ''})
+        }catch(error)
+        {
+            console.log(error);
+        }
+        
     }
     
     handleChange = event =>
     {
-        console.log(event.target);
+        //console.log(event.target);
         const{value,name} = event.target;
-        console.log([name]);
+        //console.log([name]);
         this.setState({[name]:value})
     }
 
